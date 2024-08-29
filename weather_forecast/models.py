a15377360dff4ecd9c4c21b5d3ff478684af682c
mehrdadsaadati <63453421+mehrdadsaadati@users.noreply.github.com>
2024-08-29 16:25:07 +0330

    feat: added equality check to DailyTemperatureData model

--- weather_forecast/models.py
+++ weather_forecast/models.py
@@ -14,3 +14,13 @@ class DailyTemperatureData:
         self.temperature_max = temperature_max
         self.temperature_min = temperature_min
         self.date = date
+
+    def __eq__(self, value: object) -> bool:
+        if value is DailyTemperatureData:
+            return (
+                value.temperature_max == self.temperature_max
+                and value.temperature_min == self.temperature_min
+                and value.date == self.date
+            )
+
+        return False
